ba606faa49c59c5bdf4d206e03ad7f9a119b055c
Lucas Alves Leandro <Lucas.yumi632@gmail.com>
2025-11-25 21:57:05 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -200,3 +200,29 @@ export default pool;
  * @returns {Promise<void>}
  */
 export const closePool = shutdownPool;
+
+
+
+// acessar o banco localmente sem o supabase ((fallback apeanas))
+
+// import dotenv from 'dotenv';
+// import { Pool } from 'pg';
+// dotenv.config();
+
+
+// // Debug: verificar se as variáveis estão sendo lidas <- deixar também caso o env dê errado
+// console.log(' Configurações do banco:');
+// console.log('DB_NAME:', process.env.DB_NAME);
+// console.log('DB_USER:', process.env.DB_USER);
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_PORT:', process.env.DB_PORT);
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+
+// });
+// export default pool;
